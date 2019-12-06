cf43fe386cb0bef5ccccdc1c6ccc9c21bd138abc
Bobbie Soedirgo <bobbie@soedirgo.dev>
2019-12-07 00:48:42 +0800

    first rails app thing initial commit

--- my_first_rails_app/db/schema.rb
+++ my_first_rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_063918) do
+ActiveRecord::Schema.define(version: 2019_12_06_164651) do
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
